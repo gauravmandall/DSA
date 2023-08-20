1e511c8d2f60f736ce1c7d1404916f1c7984951e
Gaurav Mandal <mandalgaurav550@gmail.com>
2023-08-20 06:25:44 +0530

    commit made on 20-08-23

---
+++ src/Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args) {
+        int rno1 = 23;
+        int rno2 = 54;
+        int rno3 = 12;
+
+//        To store in the array we can use
+//        1. Array
+        int[] rno = new int[3];
+//        or directly
+        int[] rnos = {12, 23, 54, 34, 65};
+
+    }
+}
